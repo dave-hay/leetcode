65f6853ca4038c7701382bf0762e9fd883ba9b16
hayitsdavid <davehay93@gmail.com>
2022-08-15 11:56:25 -0700

    238productOfArrayExceptSelf.js

--- 238productOfArrayExceptSelf.js
+++ 238productOfArrayExceptSelf.js
@@ -7,28 +7,19 @@
  * @return {number[]}
  */
 var productExceptSelf = function (nums) {
-  // get total product
-  let product = 0;
-  let zeroCount = 0;
+  const res = [];
 
-  for (let n of nums) {
-    if (n !== 0) product = product ? product * n : 1 * n;
-    if (n === 0) zeroCount++;
+  let product = 1;
+
+  for (let i = 0; i < nums.length; i++) {
+    res[i] = product;
+    product *= nums[i];
+  }
+  product = 1;
+  for (let j = nums.length - 1; j >= 0; j--) {
+    res[j] *= product;
+    product *= nums[j];
   }
 
-  let ans = Array(nums.length).fill(0);
-  if (zeroCount > 1) return ans; // if more than one zero return zero'd array
-
-  if (zeroCount === 1) {
-    for (let i = 0; i < nums.length; i++) {
-      // if nums[i] not 0 ignore
-      // if nums[i] is 0 add product
-      if (nums[i] === 0) ans[i] = product;
-    }
-  } else {
-    for (let i = 0; i < nums.length; i++) {
-      ans[i] = product / nums[i];
-    }
-  }
-  return ans;
+  return res;
 };

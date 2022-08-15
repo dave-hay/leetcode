57c8920dc68ecdec331a8792d2c16c5c16a7d0ea
hayitsdavid <davehay93@gmail.com>
2022-08-15 12:20:19 -0700

    238 verbose sol

--- 238productOfArrayExceptSelf.js
+++ 238productOfArrayExceptSelf.js
@@ -23,3 +23,33 @@ var productExceptSelf = function (nums) {
 
   return res;
 };
+
+/**
+ * Verbose solution
+ */
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function (nums) {
+  let left = Array(nums.length);
+
+  let product = 1;
+  for (let i = 0; i < nums.length; i++) {
+    left[i] = product;
+    product *= nums[i];
+  }
+
+  product = 1;
+  let right = Array(nums.length);
+  for (let i = nums.length - 1; i >= 0; i--) {
+    right[i] = product;
+    product *= nums[i];
+  }
+
+  let ans = Array(nums.length);
+  for (let i = 0; i < nums.length; i++) {
+    ans[i] = left[i] * right[i];
+  }
+  return ans;
+};

63489b24848ee150753b46fa8e5af0b17d6ae967
hayitsdavid <davehay93@gmail.com>
2022-08-20 10:46:19 -0700

    021mergeTwoSortedLists refactor

--- 021mergeTwoSortedLists.js
+++ 021mergeTwoSortedLists.js
@@ -1,3 +1,9 @@
+/** 21. Merge Two Sorted Lists
+ * Time complexity: O(n + m)
+ * One node of l1 and l2 is incremented on each loop iteration, the while loop runs for a number of iterations equal to the sum of the lengths of the two lists.
+ * Space complexity : O(1)
+ * The iterative approach only allocates a few pointers, so it has a constant overall memory footprint.
+ */
 /**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
@@ -10,42 +16,20 @@
  * @param {ListNode} list2
  * @return {ListNode}
  */
-var mergeTwoLists = function (list1, list2) {
-  if (!list1 && !list2) return list1;
-  if (!list1) return list2;
-  if (!list2) return list1;
+var mergeTwoLists = function (l1, l2) {
+  let nullNode = { val: 0, next: null };
+  let prev = nullNode;
 
-  let fullList;
-  if (list1.val > list2.val) {
-    fullList = list2;
-    list2 = list2.next;
-  } else {
-    fullList = list1;
-    list1 = list1.next;
-  }
-
-  // add smaller or equal
-  let fullHead = fullList;
-  while (list1 || list2) {
-    if (list1 && list2) {
-      if (list1.val > list2.val) {
-        fullList.next = list2;
-        fullList = fullList.next;
-        list2 = list2.next;
-      } else {
-        fullList.next = list1;
-        fullList = fullList.next;
-        list1 = list1.next;
-      }
-    } else if (list1) {
-      fullList.next = list1;
-      fullList = fullList.next;
-      list1 = list1.next;
-    } else if (list2) {
-      fullList.next = list2;
-      fullList = fullList.next;
-      list2 = list2.next;
+  while (l1 && l2) {
+    if (l1.val > l2.val) {
+      prev.next = l2;
+      l2 = l2.next;
+    } else {
+      prev.next = l1;
+      l1 = l1.next;
     }
+    prev = prev.next;
   }
-  return fullHead;
+  prev.next = l1 || l2;
+  return nullNode.next;
 };

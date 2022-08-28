9bcc6fdbd9a6051c3bbad2e647910d4a6deeeaa5
hayitsdavid <davehay93@gmail.com>
2022-08-28 16:50:07 -0700

    235lowestCommonAncestorOfBST.py

---
+++ 235lowestCommonAncestorOfBST.py
@@ -0,0 +1,55 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+
+    def lcaIterative(self, root, p, q):
+        # Value of p
+        p_val = p.val
+
+        # Value of q
+        q_val = q.val
+
+        # Start from the root node of the tree
+        node = root
+
+        # Traverse the tree
+        while node:
+
+            # Value of current node or parent node.
+            parent_val = node.val
+
+            if p_val > parent_val and q_val > parent_val:
+                # If both p and q are greater than parent
+                node = node.right
+            elif p_val < parent_val and q_val < parent_val:
+                # If both p and q are lesser than parent
+                node = node.left
+            else:
+                # We have found the split point, i.e. the LCA node.
+                return node
+
+    def lcaRecursive(self, root, p, q):
+        # Value of current node or parent node.
+        parent_val = root.val
+
+        # Value of p
+        p_val = p.val
+
+        # Value of q
+        q_val = q.val
+
+        # If both p and q are greater than parent
+        if p_val > parent_val and q_val > parent_val:
+            return self.lcaRecursive(root.right, p, q)
+        # If both p and q are lesser than parent
+        elif p_val < parent_val and q_val < parent_val:
+            return self.lcaRecursive(root.left, p, q)
+        # We have found the split point, i.e. the LCA node.
+        else:
+            return root

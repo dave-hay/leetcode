a35a569420c29e8b14d22e2fb866d895fbc52ad0
hayitsdavid <davehay93@gmail.com>
2022-09-14 14:09:54 -0700

    797allPathsFromSourceToTarget.py dfs

--- 797allPathsFromSourceToTarget.py
+++ 797allPathsFromSourceToTarget.py
@@ -1,6 +1,42 @@
+from collections import deque
+
+
 class Solution:
 
-    def allPathsSourceTarget(self, graph: list[list[int]]) -> list[list[int]]:
+    def allPathsSourceTargetBfs(self,
+                                graph: list[list[int]]) -> list[list[int]]:
+        """
+        Time Complexity: O(2^V * V)
+            Graph with V verticies mean 2^(V-1) - 1 possible paths from start to end
+            O(V) time to build each path
+            (2^V-1 - 1) * O(V) = O(2^V * V)
+        Space Complecity: O(2^V * V)
+            queue contains at most O(2^V) paths
+            each path will take O(V) space
+        """
+        paths = []
+        if not graph or len(graph) == 0:
+            return paths
+
+        queue = deque()
+        path = [0]
+        queue.append(path)
+
+        while queue:
+            current_path = queue.popleft()
+            node = current_path[-1]
+            for next_node in graph[node]:
+                temp_path = current_path.copy()
+                temp_path.append(next_node)
+
+                if next_node == len(graph) - 1:
+                    paths.append(temp_path)
+                else:
+                    queue.append(temp_path)
+        return paths
+
+    def allPathsSourceTargetDfs(self,
+                                graph: list[list[int]]) -> list[list[int]]:
         """
         Time Complexity: O(2^V * V)
             DAG can have 2^V-1 - 1 possible paths

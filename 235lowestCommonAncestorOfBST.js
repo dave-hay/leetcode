a7af496788266309871d94ba0934cdd1d5c97c67
hayitsdavid <davehay93@gmail.com>
2022-10-20 12:10:12 -0700

    235 optimized js

--- 235lowestCommonAncestorOfBST.js
+++ 235lowestCommonAncestorOfBST.js
@@ -11,19 +11,49 @@
  * @param {TreeNode} p
  * @param {TreeNode} q
  * @return {TreeNode}
+ * Time Complexity: O(N) - Where N is the height of the tree
+ * Space Complexity: O(1)
  */
-// inOrder traversal?
-// once the ans has been hit keep going to parent until it's the same
-// since binary tree know if smaller traverse left and bigger go right
-var lowestCommonAncestor = function (root, p, q) {
+var lowestCommonAncestorIterative = function (root, p, q) {
+  const pVal = p.val;
+  const qVal = q.val;
+
+  let node = root;
+
+  while (node) {
+    let nVal = node.val;
+
+    if (nVal > pVal && nVal > qVal) {
+      node = node.left;
+      continue;
+    }
+
+    if (nVal < pVal && nVal < qVal) {
+      node = node.right;
+      continue;
+    }
+
+    return node;
+  }
+};
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ * Time Complexity: O(N) - Where N is the height of the tree
+ * Space Complexity: O(N) - Recusive call stack
+ */
+var lowestCommonAncestorRecursive = function (root, p, q) {
   let curVal = root.val;
   let pVal = p.val;
   let qVal = q.val;
 
   if (curVal > pVal && curVal > qVal) {
-    return lowestCommonAncestor(root.left, p, q);
+    return lowestCommonAncestorRecursive(root.left, p, q);
   } else if (curVal < pVal && curVal < qVal) {
-    return lowestCommonAncestor(root.right, p, q);
+    return lowestCommonAncestorRecursive(root.right, p, q);
   } else {
     return curVal;
   }

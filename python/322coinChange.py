4b0681b0c74f9a75ecc21796499ab970a588817e
Dave Hay <davehay93@gmail.com>
2023-02-22 12:57:17 -0800

    322 iterative approach py

--- python/322coinChange.py
+++ python/322coinChange.py
@@ -2,6 +2,27 @@ from functools import lru_cache
 
 
 class Solution:
+    """
+    iterative approach
+    time: O(amount * len(coins))
+    space: O(amount)
+    """
+
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [float("inf") for _ in range(amount + 1)]
+        dp[0] = 0
+
+        for a in range(1, amount + 1):
+            for c in coins:
+                if a - c >= 0:
+                    dp[a] = min(dp[a], 1 + dp[a - c])
+
+        if dp[amount] == float("inf"):
+            return -1
+        return dp[amount]
+
+
+class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         if amount == 0:
             return 0

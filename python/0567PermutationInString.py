947cf4c75ee998ec734cce0ad2fbb1c958c07345
Dave Hay <davehay93@gmail.com>
2023-03-27 11:21:03 -0700

    567 py v2

--- python/0567PermutationInString.py
+++ python/0567PermutationInString.py
@@ -1,6 +1,56 @@
 class Solution:
     """
     sliding window
+    n1, n2 = len(s1), len(s2)
+    t: O(n1 + (n2 - n1))
+    s: O(1)
+    """
+
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+
+        s1Count, s2Count = [0] * 26, [0] * 26
+
+        # first window to match
+        for i in range(len(s1)):
+            s1Count[ord(s1[i]) - ord("a")] += 1
+            s2Count[ord(s2[i]) - ord("a")] += 1
+
+        matches = 0
+
+        for i in range(26):
+            if s1Count[i] == s2Count[i]:
+                matches += 1
+
+        l = 0
+        for r in range(len(s1), len(s2)):
+            if matches == 26:
+                return True
+
+            idx = ord(s2[r]) - ord("a")
+            s2Count[idx] += 1
+            if s1Count[idx] == s2Count[idx]:
+                matches += 1
+            elif s1Count[idx] + 1 == s2Count[idx]:
+                matches -= 1
+
+            idx = ord(s2[l]) - ord("a")
+            s2Count[idx] -= 1
+            if s1Count[idx] == s2Count[idx]:
+                matches += 1
+            elif s1Count[idx] - 1 == s2Count[idx]:
+                matches -= 1
+            l += 1
+        return matches == 26
+
+
+class Solution:
+    """
+    sliding window
+    n1, n2 = len(s1), len(s2)
+    t: O(n1 + 26(n2 - n1))
+    s: O(1)
     """
 
     def checkInclusion(self, s1: str, s2: str) -> bool:
@@ -28,12 +78,3 @@ class Solution:
             if mp1[i] != mp2[i]:
                 return False
         return True
-
-
-s = Solution()
-res = s.checkInclusion("ab", "eidboaoo")
-print(res)
-# there are len(s2) // len(s1) options
-
-res = s.checkInclusion("ab", "eidbaooo")
-print(res)

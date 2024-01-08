3b4ff0303b314e28acf9b885e8ad3b8e8ac42cf9
dave-hay <davehay93@gmail.com>
2024-01-08 14:45:59 -0800

    21 updated

--- python/0021mergeTwoSortedLists.py
+++ python/0021mergeTwoSortedLists.py
@@ -13,6 +13,29 @@ class Solution:
         creating one new ListNode then redirecting pointers
     """
 
+    def mergeTwoListsEst(
+        self, list1: Optional[ListNode], list2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+        head = ListNode()
+        node = head
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                node.next = list1
+                list1 = list1.next
+            else:
+                node.next = list2
+                list2 = list2.next
+            node = node.next
+
+        if list1:
+            node.next = list1
+
+        if list2:
+            node.next = list2
+
+        return head.next
+
     def mergeTwoListsEzr(
         self, list1: Optional[ListNode], list2: Optional[ListNode]
     ) -> Optional[ListNode]:

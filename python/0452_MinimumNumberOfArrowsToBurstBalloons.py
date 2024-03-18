81cf9b5df32313dd8b68144b89376043a75097f2
dave-hay <davehay93@gmail.com>
2024-03-18 14:14:15 -0700

    452. Minimum Number of Arrows to Burst Balloons

--- python/0452_MinimumNumberOfArrowsToBurstBalloons.py
+++ python/0452_MinimumNumberOfArrowsToBurstBalloons.py
@@ -1,6 +1,5 @@
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
-
         points.sort(key=lambda x: x[0])
         res = len(points)
         prev = points[0]
@@ -17,45 +16,3 @@ class Solution:
                 prev = p
 
         return res
-
-
-class Solution:
-    def findMinArrowShots(self, points: List[List[int]]) -> int:
-
-        points.sort(key=lambda x: x[0])
-        res = [points[0]]
-
-        for p in points[1:]:
-            a_start, a_end = res[-1]
-            b_start, b_end = p
-
-            # know a_start <= b_start
-            if b_start <= a_end:
-                res[-1] = [a_start, min(a_end, b_end)]
-            else:
-                res.append(p)
-
-        return len(res)
-
-
-class Solution:
-    def findMinArrowShots(self, points: List[List[int]]) -> int:
-
-        points.sort(key=lambda x: x[0])
-        res = [points[0]]
-
-        for p in points[1:]:
-            a_start, a_end = res[-1]
-            b_start, b_end = p
-
-            if (
-                a_start <= b_start <= a_end
-                or a_start <= b_end <= a_end
-                or b_start <= a_start <= b_end
-                or b_start <= a_end <= b_end
-            ):
-                res[-1] = [max(a_start, b_start), min(a_end, b_end)]
-            else:
-                res.append(p)
-
-        return len(res)

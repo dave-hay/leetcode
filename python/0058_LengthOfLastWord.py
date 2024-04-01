07767a06f8665879267e9ee1fd62d95691568ec6
dave-hay <davehay93@gmail.com>
2024-03-31 17:29:27 -0700

    alt 0058

--- python/0058_LengthOfLastWord.py
+++ python/0058_LengthOfLastWord.py
@@ -8,3 +8,17 @@ class Solution:
                 l -= 1
 
             return r - l
+
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        end = len(s) - 1
+
+        while s[end] == " ":
+            end -= 1
+
+        start = end
+        while start >= 0 and s[start] != " ":
+            start -= 1
+
+        return end - start
